89c0866d121a9b04f0504405ee430fb4a7d67325
Kurisu44 <Kurisu44@users.noreply.github.com>
2014-08-26 23:19:09 -0700

    Not done lol.

---
+++ K4Nami/Nami.cs
@@ -0,0 +1,59 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K4Nami
+{
+    class Nami
+    {
+        public static Orbwalking.Orbwalker orbwalker;
+        public static Obj_AI_Hero Player = ObjectManager.Player;
+        public static Spellbook SB = Player.Spellbook;
+
+        public static float QDelay = 0.5f;
+        public static float QSpeed = 1750f;
+        public static float QWidth = 200f;
+        public static float QRange = 875f;
+
+        public static Spell Q = new Spell(SpellSlot.Q, 875);
+        public static Spell W = new Spell(SpellSlot.W, 725);
+        public static Spell E = new Spell(SpellSlot.E, 800);
+
+        public static void SetQ()
+        {      
+            Q.SetSkillshot(QDelay, QWidth, QSpeed, false, Prediction.SkillshotType.SkillshotCircle);
+        }
+
+
+        public static void CastCombo(Obj_AI_Base target) 
+        {
+            CastQ(target);
+            CastW(target);
+            CastE(target);
+        }
+
+        public static void CastQ(Obj_AI_Base target)
+        {
+
+        }
+
+        public static void CastW(Obj_AI_Base target)
+        {
+
+        }
+
+        public static void CastE(Obj_AI_Base target)
+        {
+
+        }
+
+
+
+    }
+
+   
+}
---
+++ K4Nami/Namirino.cs
@@ -0,0 +1,84 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+
+namespace K4Nami
+{
+    internal class Namirino
+    {
+        public const string CharName = "Nami";
+        public static Menu Config;
+        public static Obj_AI_Hero target;
+
+        public Namirino()
+        {
+            CustomEvents.Game.OnGameLoad += init;
+        }
+
+        #region Nami Menu
+        private static void init(EventArgs args)
+        {
+            Game.PrintChat("Nami - the Tidecaller by K44");
+
+            try
+            {
+                Config = new Menu("Nami - the Tidecaller", "Nami", true);
+
+                Config.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+                Nami.orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalker"));
+
+                Config.AddSubMenu(new Menu("Nami Combo", "combo"));
+                Config.SubMenu("combo").AddItem(new MenuItem("useQ", "Use Q")).SetValue(true);
+                Config.SubMenu("combo").AddItem(new MenuItem("useW", "Use W")).SetValue(false);
+                Config.SubMenu("combo").AddItem(new MenuItem("useE", "Use E")).SetValue(true);
+
+                Config.AddSubMenu(new Menu("Nami Harass", "harass"));
+                Config.SubMenu("harass").AddItem(new MenuItem("useQ", "Use Q")).SetValue(true);
+                Config.SubMenu("harass").AddItem(new MenuItem("useW", "Use W")).SetValue(false);
+                Config.SubMenu("harass").AddItem(new MenuItem("useE", "Use E")).SetValue(false);
+
+            }
+            catch
+            {
+                Game.PrintChat("Error loading script, contact dev!");
+            }
+        }
+        #endregion
+
+        #region OnGameUpdate
+
+        private static void OnGameUpdate(EventArgs args)
+        {
+            if (Nami.orbwalker.ActiveMode.ToString() == "Combo")
+            {
+                target = SimpleTs.GetTarget(1150, SimpleTs.DamageType.Magical);
+                Nami.CastCombo(target);
+            }
+
+            if (Nami.orbwalker.ActiveMode.ToString() == "Mixed")
+            {
+
+            }
+
+            if (Nami.orbwalker.ActiveMode.ToString() == "LaneClear")
+            {
+
+            }
+
+        }
+
+        #endregion
+
+
+
+
+
+
+    }
+}
