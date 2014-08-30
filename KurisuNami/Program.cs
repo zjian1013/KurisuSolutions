34dcd6529dddd8c5e7605226463b139621938a46
Kurisu44 <Kurisu44@users.noreply.github.com>
2014-08-29 22:58:51 -0700

    Base

---
+++ KurisuNami/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurisuNami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Nami();
+        }
+    }
+}
